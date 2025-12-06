6badbc767a2698999be777cf3ea24b4ca3028610
Marcelle Miyake Cardille <marcelle.mcardille@usp.br>
2025-12-05 23:23:57 -0300

    comment, admin e migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
 from django.contrib import admin
-from .models import Post
-# Register your models here.
-admin.site.register(Post)
+from .models import Post, Comment
 
+admin.site.register(Post)
+admin.site.register(Comment)
 
--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,29 @@
-from django.db import models
 
-# Create your models here.
 from django.db import models
+from django.contrib.auth import get_user_model 
+
+
+User = get_user_model() 
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField() 
     date_posted = models.DateTimeField(auto_now_add=True)
-
+    logo = models.CharField(max_length=255, blank=True)
     def __str__(self):
         return self.title
 
-#class Post(models.Model):
-    #title = models.CharField(max_length=200, verbose_name="Título")
-    #content = models.TextField(verbose_name="Conteúdo HTML")
-    #date_posted = models.DateTimeField(auto_now_add=True, verbose_name="Data de Postagem")
 
-    #class Meta:
-       
-        #ordering = ['-date_posted']
-        #verbose_name = "Postagem"
-        #verbose_name_plural = "Postagens"
+class Comment(models.Model):   
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    texto = models.TextField(verbose_name="Comentário")
+    data = models.DateTimeField(auto_now_add=True) 
+
+    def __str__(self):
+        return f'Comentário de {self.autor} em {self.post.title}'
+
+    class Meta:
+        ordering = ['data']
+        verbose_name = "Comentário"
+        verbose_name_plural = "Comentários"
